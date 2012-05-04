0b8b0960c0f7e1733e258e04a1dadf9cf9f38cea
Dimitri Lebel <Dimitri Lebel@DimitriLebel-PC.langlade.biz.>
2012-05-04 15:52:36 +0200

    Réorganisation du projet 3eme

--- TimeReportFx/src/timereportfx/models/TimeReport.java
+++ TimeReportFx/src/timereportfx/models/TimeReport.java
@@ -4,10 +4,85 @@
  */
 package timereportfx.models;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 /**
  *
  * @author Dimitri Lebel
  */
 public class TimeReport {
-    
+
+    private IntegerProperty idtimereport;
+    private StringProperty tsDebut;
+    private StringProperty tsFin;
+    private IntegerProperty duree;
+    private Utilisateur utilisateur;
+    private Tache tache;
+
+    public TimeReport() {
+        idtimereport = new SimpleIntegerProperty(-1);
+        tsDebut = new SimpleStringProperty("");
+        tsFin = new SimpleStringProperty("");
+        utilisateur = new Utilisateur();
+        tache = new Tache();
+    }
+
+    public IntegerProperty DureeProperty() {
+        return duree;
+    }
+
+    public void setDuree(int duree) {
+        this.duree.set(duree);
+    }
+
+    public Integer getDuree() {
+        return duree.get();
+    }
+
+    public IntegerProperty IdtimereportProperty() {
+        return idtimereport;
+    }
+
+    public Integer getIdtimereport() {
+        return idtimereport.get();
+    }
+
+    public void setIdtimereport(int idtimereport) {
+        this.idtimereport.set(idtimereport);
+    }
+
+    public Tache getTache() {
+        return tache;
+    }
+
+    public void setTache(Tache tache) {
+        this.tache = tache;
+    }
+
+    public StringProperty getTsDebut() {
+        return tsDebut;
+    }
+
+    public void setTsDebut(StringProperty tsDebut) {
+        this.tsDebut = tsDebut;
+    }
+
+    public StringProperty getTsFin() {
+        return tsFin;
+    }
+
+    public void setTsFin(StringProperty tsFin) {
+        this.tsFin = tsFin;
+    }
+
+    public Utilisateur getUtilisateur() {
+        return utilisateur;
+    }
+
+    public void setUtilisateur(Utilisateur utilisateur) {
+        this.utilisateur = utilisateur;
+    }
 }
